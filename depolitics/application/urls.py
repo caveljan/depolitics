96ddd159456fd8ef73dee313fbc1663da981752b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-01-25 13:35:37 +0200

    Dev: add Ajax post/response

--- depolitics/application/urls.py
+++ depolitics/application/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/', views.add_name, name='add_name'),
+    path('search/', views.search_id_string, name='search_id_string')
 ]
