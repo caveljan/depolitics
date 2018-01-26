26f38b3044a0cc1e390c3d7d286173d526afbb2e
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-01-26 10:09:09 +0200

    Dev: api setup

--- depolitics/application/urls.py
+++ depolitics/application/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add_name, name='add_name'),
-    path('search/', views.search_id_string, name='search_id_string')
+    path('search/', views.search_id_string, name='search_id_string'),
+    path('api/database', views.api_database, name='api_database'),
+    path('api/search', views.api_search, name='api_search')
 ]
