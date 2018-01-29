fed5e774e7b9d82fb47fea07f4b27480e4cda755
Jan Cavel <caveljan@gmail.com>
2018-01-29 15:31:26 +0200

    Dev: partial performance match replace

--- extensions/chrome/js/contentscript/contentscript.js
+++ extensions/chrome/js/contentscript/contentscript.js
@@ -12,33 +12,6 @@ chrome.storage.sync.get(['activeOptions', 'database'], function(element) {
 
 
 function _main(options, database) {
-    // Ben Alman's replaceText plugin
-    // http://www.benalman.com/projects/jquery-replacetext-plugin/
-    $.fn.replaceText = function(search, replace, text_only) {
-        return this.each(function(){
-            var node = this.firstChild,
-                val,
-                new_val,
-                remove = [];
-            if ( node ) {
-                do {
-                    if ( node.nodeType === 3 ) {
-                        val = node.nodeValue;
-                        new_val = val.replace( search, replace );
-                        if ( new_val !== val ) {
-                            if ( !text_only && /</.test( new_val ) ) {
-                                $(node).before( new_val );
-                                remove.push( node );
-                            } else {
-                                node.nodeValue = new_val;
-                            }
-                        }
-                    }
-                } while ( node = node.nextSibling );
-            }
-            remove.length && $(remove).remove();
-        });
-    };
 
     if (/wikipedia/.test(document.location.href)) {
         var isWiki = true;
