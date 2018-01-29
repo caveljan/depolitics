332aabc34fb63d8f7cabd67ee582233b69097360
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-01-29 15:31:26 +0200

    Dev: partial performance match replace

--- extensions/chrome/js/contentscript/components/replace-text.js
+++ extensions/chrome/js/contentscript/components/replace-text.js
@@ -28,22 +28,92 @@ $.fn.replaceText = function(search, replace, text_only) {
 
 
 function replaceText(text, database) {
-    console.log("array of text", text);
-    console.log("database", database);
+    // console.log("array of text", text);
+    // console.log("database", database);
 
-    for (let i=0; i < text.length; i++) {
-        for(let j=0; j < database.length; j++) {
-            if (database[j]["last_name"] == text[i]) {
-                let identificationString = database[j]["identification_string"];
+    let textSet = new Set(text);
+    // console.log(textSet);
 
-                if (database[j]["first_name"] == text[i-1]) {
-                    let politicianFullName = text[i-1] + " " + text[i];
-                    $("*").replaceText(politicianFullName, identificationString);
-                } else {
-                    let politicianLastName = text[i];
-                    $("*").replaceText(politicianLastName, identificationString);
-                }
+    let databaseLastNames = new Set()
+    for (let i=0; i < database.length; i++) {
+        databaseLastNames.add(database[i]["last_name"]);
+    }
+    // console.log(databaseLastNames);
+
+    let textDatabaseIntersection = new Set([...textSet].filter(x => databaseLastNames.has(x)));
+    console.log(textDatabaseIntersection)
+
+
+    for(let politicianLastName of textDatabaseIntersection) {
+        // console.log(politicianLastName);
+        for (let i=0; i <database.length; i++) {
+            if (database[i]["last_name"] == politicianLastName) {
+                let identificationString = database[i]["identification_string"];
+
+                // makes an array out of name_variants, removing commas and spaces 
+                let name_variants = database[i]["name_variants"]
+                                                        .split(",")
+                                                        .map(item => item.trim());
+                // console.log(name_variants);
+
+                // for (let k=0; k < name_variants.length; k++) {
+                //     name_variant = name_variants[k].split(" ");
+                //     let length = name_variant.length;
+                //     if (name_variant[length - 1] == text[i] &&
+                //         name_variant[length - 2] == text[i-1] &&
+                //         name_variant[length - 3] == text[i-2]) {
+                //         // console.log(k, name_variant);
+                //         $("*").replaceText(name_variants[k], identificationString);
+                //     }
+                // }
+                
+                // if (database[i]["first_name"] == text[i-1]) {
+                //     let politicianFullName = text[i-1] + " " + text[i];
+                //     $("*").replaceText(politicianFullName, identificationString);
+                // }
+
+                // if (database[i]["first_name"] != text[i-1]) {
+                    // let politicianLastName = text[i];
+                    $("*").replaceText(politicianLastName, identificationString);                    
+                // }
             }
         }
     }
+
+
+
+    // for (let i=0; i < text.length; i++) {
+    //     for(let j=0; j < database.length; j++) {
+    //         if (database[j]["last_name"] == text[i]) {
+    //             let identificationString = database[j]["identification_string"];
+
+    //             // makes an array out of name_variants, removing commas and spaces 
+    //             let name_variants = database[j]["name_variants"]
+    //                                                     .split(",")
+    //                                                     .map(item => item.trim());
+    //             // console.log(name_variants);
+
+    //             for (let k=0; k < name_variants.length; k++) {
+    //                 name_variant = name_variants[k].split(" ");
+    //                 let length = name_variant.length;
+    //                 if (name_variant[length - 1] == text[i] &&
+    //                     name_variant[length - 2] == text[i-1] &&
+    //                     name_variant[length - 3] == text[i-2]) {
+    //                     // console.log(k, name_variant);
+    //                     $("*").replaceText(name_variants[k], identificationString);
+    //                 }
+    //             }
+                
+    //             if (database[j]["first_name"] == text[i-1]) {
+    //                 let politicianFullName = text[i-1] + " " + text[i];
+    //                 $("*").replaceText(politicianFullName, identificationString);
+    //             }
+
+    //             if (database[j]["first_name"] != text[i-1]) {
+    //                 let politicianLastName = text[i];
+    //                 $("*").replaceText(politicianLastName, identificationString);                    
+    //             }
+    //         }
+    //     }
+    // }
 }
