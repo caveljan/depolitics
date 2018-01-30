6e83742576275515eccc4de87360953650334fcd
Jan Cavel <caveljan@gmail.com>
2018-01-30 15:45:27 +0200

    Dev: Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Depolitics
+
+[depolitics.org](https://depolitics.org)
+
+Replace the Name of politicians with a random string of characters.
--- extensions/chrome/js/contentscript/components/database.js
+++ extensions/chrome/js/contentscript/components/database.js
@@ -1,6 +1,6 @@
 function getDatabase() {
     let xhr = new XMLHttpRequest();
-    let url = "http://127.0.0.1:8080/api/database";
+    let url = "https://depolitics.org/api/database";
 
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
