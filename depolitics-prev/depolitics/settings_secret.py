c2eaffe1b5425954e64c3454593380978f4e5347
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-04-25 21:40:04 +0300

    Setup: cleanup

--- depolitics-prev/depolitics/settings_secret.py
+++
@@ -1,9 +0,0 @@
-secret_SECRET_KEY = ')o(33d*p8bf#brg=k+0fl28n9na+ubz%t7hlr^y@nr6e(eha8s'
-# 'vjm)n63vg)3j7qvofi55#2o46#^)ljou@pwvij5)is4u)r*)@&'
-
-secret_DB_ENGINE = 'django.db.backends.postgresql_psycopg2'
-secret_DB_NAME = 'depolitics'
-secret_DB_USER = 'caveljan'
-secret_DB_PASSWORD = '1234%qwerT'
-secret_DB_PORT = '5432'
-secret_DB_CLOUD = '/cloudsql/depolitics-org:us-central1:depolitics-org'
